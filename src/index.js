ef9dbc4b6023e66421f42f9b85dc318c71a2e9b0
Aurelius Wendelken <webtobesocial@gmail.com>
2023-09-30 12:44:28 +0200

    introduce routing controller to core

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Model from './lib/model.js'
 import View from './lib/view.js'
+import Router from './lib/router.js'
 import Controller from './lib/controller.js'
 import Component from './lib/component.js'
 import GridComponent from './lib/gridComponent.js'
@@ -7,6 +8,7 @@ import GridComponent from './lib/gridComponent.js'
 export {
     Model,
     View,
+    Router,
     Controller,
     Component,
     GridComponent
