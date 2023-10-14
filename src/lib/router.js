26aa7c90470f69c1125161a5c0fba64afb8b81dc
Roald Christesen <roald@grain.one>
2023-10-14 19:10:26 +0200

    renamed variables regarding by reviewer

--- src/lib/router.js
+++ src/lib/router.js
@@ -24,27 +24,27 @@ export default class Router {
      * Pop state events are fired from the browser, ie. when user goes back and forth in the browser hostory.
      */
     handlePopState() {
-        this.loadRouteByName(event.state.slug, false)
+        this.loadRouteByName(event.state.name, false)
     }
 
     /**
      * Get a route by its name.
      *
-     * @param {string} slug - Name of route to find.
+     * @param {string} routeName - Name of route to find.
      * @return {object} The route or null.
      */
-    getRouteByName(slug) {
-        return this.routes.find((obj) => obj.slug === slug)
+    getRouteByName(routeName) {
+        return this.routes.find((route) => route.name === routeName)
     }
 
     /**
      * Load a route by its name.
      *
-     * @param {string} slug - Name of route to load.
+     * @param {string} routeName - Name of route to load.
      * @param {boolean} pushFlag - If true, new state will be pushed to window.history
      */
-    loadRouteByName(slug, pushFlag = true) {
-        this.loadRoute(this.getRouteByName(slug), pushFlag)
+    loadRouteByName(routeName, pushFlag = true) {
+        this.loadRoute(this.getRouteByName(routeName), pushFlag)
     }
 
     /**
@@ -55,7 +55,7 @@ export default class Router {
      */
     loadRoute(route, pushFlag = true) {
         if (route !== null) {
-            const container = document.querySelector(route.injectionPoint) // Todo, helper nutzen und root von außen!
+            const container = document.querySelector(route.inject) // Todo, helper nutzen und root von außen!
 
             while (container.firstChild) {
                 container.removeChild(container.firstChild)
@@ -66,10 +66,10 @@ export default class Router {
 
                 if (pushFlag === true) {
                     const state = {
-                        slug: route.slug,
+                        name: route.name,
                         title: route.title,
-                        path: route.path,
-                        injectionPoint: route.injectionPoint
+                        inject: route.inject,
+                        path: route.path
                     }
 
                     window.history.pushState(state, route.title, route.path)
